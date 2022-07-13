0081d1e7ef010e0c5c93cc57129b73466bcc0732
DavidGtzDev <davidgtzleal@gmail.com>
2022-07-13 12:06:30 -0600

    Still working on mint UI

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -1,14 +1,19 @@
 import AppleLogo from "../../assets/580b57fcd9996e24bc43c516.png"
 import LineSVG from "../../assets/line.svg"
+import StockIMG from "../../assets/icons8-stocks-growth-48.png"
 
 export default function CompanyCard() {
   return (
     <div className="company-card">
-        <img src={AppleLogo}></img>
+      <img src={AppleLogo} className="company-card-logo-img"></img>
+      <div className="company-card-subdiv-company-name">
         <h2>Apple</h2>
-        <img src={LineSVG}></img>
-
-
+      </div>
+      <div className="company-card-subdiv-decoration-line"></div>
+      <div className="company-card-subdiv-company-status">
+        <img src={StockIMG}></img>
+        <h2>10% buy since last week</h2>
+      </div>
     </div>
-  )
+  );
 }
